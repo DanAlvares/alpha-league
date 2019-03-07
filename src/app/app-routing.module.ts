5899d81c2411b8d87c23c15580bed144bb2424b5
Dan Alvares <dm.alvares@gmail.com>
2019-03-07 19:04:09 +0000

    Setup TeamsListing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TeamsListingComponent } from './teams/teams-listing/teams-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'teams',
+    pathMatch: 'full'
+  },
+  {
+    path: 'teams',
+    component: TeamsListingComponent
+  },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
